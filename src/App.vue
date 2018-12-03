ff879a480378cd96854e6b1ed11f959f9f24f1c7
Student <student@d17-l428-pc-10.ipa.iisg.agh.edu.pl>
2018-12-03 09:33:56 +0100

    vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>Clicked {{ times }} times</h1>
+    <button @click="iAmClicked()">Click me!</button>
+    <button @click="Reset()">reset</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        data() {
+            return {times: 0}
+        },
+        methods: {
+            iAmClicked() {
+                this.times = this.times + 1;
+            },
+            Reset(){
+                this.times = 0;
+            }
+        }
+    };
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
